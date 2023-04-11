fa4efb8a0126c2f154e1bb51c3f6315ddb26b530
micchecka <122084453+micchecka@users.noreply.github.com>
2023-04-11 12:21:47 +0300

    Update UserRepository.java

--- src/main/java/Spring321/SpringApp/repository/UserRepository.java
+++ src/main/java/Spring321/SpringApp/repository/UserRepository.java
@@ -1,10 +1,8 @@
 package Spring321.SpringApp.repository;
 
-
-
 import Spring321.SpringApp.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {  }
